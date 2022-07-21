5db5ac3c0087c3efb920f5c3057189c34eaa7093
lamyeemanick <nick.lam-yee-man@epitech.eu>
2022-07-21 18:00:08 +0400

    FIX: Disable Modal for add job

--- src/components/Button/AddJobOffer/AddJobOfferButton.jsx
+++ src/components/Button/AddJobOffer/AddJobOfferButton.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 
-import AddJobOfferModal from "../../Modal/AddJob/AddJobModal";
+// import AddJobOfferModal from "../../Modal/AddJob/AddJobModal";
 
 const AddJobOfferButton = () => {
   const {visitor} = useSelector(state => state.visitor);
@@ -11,7 +11,7 @@ const AddJobOfferButton = () => {
       <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" hidden={hidden} type="button" data-modal-toggle="add-job-modal">
         Add Job Offer
       </button>
-      <AddJobOfferModal/>
+      {/* <AddJobOfferModal/> */}
     </div>
   );
 }
