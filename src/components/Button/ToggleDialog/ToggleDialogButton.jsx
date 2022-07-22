f87cf11c2d875d496a965e0b634029339c2bc947
lamyeemanick <nick.lam-yee-man@epitech.eu>
2022-07-22 12:31:32 +0400

    STYLE: Toggle Dialog Button changed to primary button style

--- src/components/Button/ToggleDialog/ToggleDialogButton.jsx
+++ src/components/Button/ToggleDialog/ToggleDialogButton.jsx
@@ -2,9 +2,9 @@ const ToggleDialogButton = (props) => {
   return (
     <div>
       <button
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-6 py-3.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         hidden={props.hidden}
-        onClick={props.onClick}      
+        onClick={props.onClick}
       >
         {props.label}
       </button>
